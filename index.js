9b569e6c249cb08e87106af7bf25e2a949eed20f
galactic-shark <evamichael100@gmail.com>
2023-07-04 16:33:29 +0800

    updated delete function to remove items from db

--- index.js
+++ index.js
@@ -13,71 +13,68 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
-const inputEl = document.getElementById("input-field")
-const addBtn = document.getElementById("add-button")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
-addBtn.addEventListener("click", function () {
-    let inputField = inputEl.value
-    if (inputField) {
 
-        // Push to the db
-        push(shoppingListInDB, inputField)
 
-        clearInput()
+addButtonEl.addEventListener("click", function () {
+    let inputValue = inputFieldEl.value
+    if (inputValue) {
+        push(shoppingListInDB, inputValue)
+
+        clearInputFieldEl()
     }
-
 })
+
 
 //accessing and processing data from db
 
 // converting DB object into an array
 onValue(shoppingListInDB, function (snapshot) {
-
     if (snapshot.exists()) {
-
         //creates an array called shoppingList with the key & value pairs (Object.entries)
-        let shoppingList = Object.entries(snapshot.val())
-
+        let itemsArray = Object.entries(snapshot.val())
         //need to add this so that all array items aren't rendered twice
         clearShoppingListEl()
 
         // iterating through array and passing the individualItem through the render function to render it 
-        for (let i = 0; i < shoppingList.length; i++) {
-            let currentItem = shoppingList[i]
-            let currentItemId = currentItem[0]
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
             let currentItemValue = currentItem[1]
-            // console.log(currentItem)
-            render(currentItem)
-        }
 
+            appendItemToShoppingListEl(currentItem)
+        }
     } else {
         shoppingListEl.innerHTML = "No items here... yet"
     }
-
 })
 
-function render(item) {
-    let itemId = item[0]
+
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
     let itemValue = item[1]
+
     let newEl = document.createElement("li")
+
     newEl.textContent = itemValue
 
-
     newEl.addEventListener("click", function () {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemValue}`)
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
 
         remove(exactLocationOfItemInDB)
-        // console.log(itemValue)
-        // console.log(shoppingList)
-
     })
+
     shoppingListEl.append(newEl)
 }
-
-function clearInput() {
-    inputEl.value = null
-}
 function clearShoppingListEl() {
-    shoppingListEl.innerHTML = null
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
 }
